817d887c5aa3bcc7750df8c8c6beb09bd6f25c92
Chi Nguyen Ngoc <ngocchi.fit148.vnu@gmail.com>
2021-10-07 15:33:30 +0700

    add grid-stack

--- main.js
+++ main.js
@@ -25,9 +25,9 @@ const myChart = new Chart(document.getElementById("chart"), config);
 // handle event
 const changeSizeButton = document.getElementById("change-size-btn");
 // initial width and height
-const chartContainer = document.getElementsByClassName("chart-container")[0];
-chartContainer.style.width = `1000px`;
-chartContainer.style.height = `500px`;
+// const chartContainer = document.getElementsByClassName("chart-container")[0];
+// chartContainer.style.width = `1000px`;
+// chartContainer.style.height = `500px`;
 changeSizeButton.addEventListener("click", () => {
   const width = document.getElementById("width").value;
   const height = document.getElementById("height").value;
@@ -36,3 +36,5 @@ changeSizeButton.addEventListener("click", () => {
     chartContainer.style.height = `${height}px`;
   }
 });
+
+GridStack.init();
